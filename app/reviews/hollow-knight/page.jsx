57cc08d4252a71d148989d95dd31a61fe6b46ffc
Glenn Pinto <gvpinto@gmail.com>
2023-08-02 23:09:00 -0400

    Added images

--- app/reviews/hollow-knight/page.jsx
+++ app/reviews/hollow-knight/page.jsx
@@ -4,6 +4,7 @@ function HollowKnightPage() {
     return (
         <>
             <Header>Hollow Knight</Header>
+            <img src="/images/hollow-knight.jpg" alt="" className="mb-3 rounded" width="640" height="360" />
             <p>Hollow Knight is my favorite review</p>
         </>
     );
--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,7 @@ function StardewValleyPage() {
     return (
         <>
             <Header>Stardew Valley</Header>
+            <img src="/images/stardew-valley.jpg" alt="" className="mb-3 rounded" width="640" height="360" />
             <p>
                 This will be the review for Stardew Valley
             </p>
