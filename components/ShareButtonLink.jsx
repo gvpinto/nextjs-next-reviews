5efd9ee52269c53fa15e101efde1102180ff17e8
Glenn Pinto <gvpinto@gmail.com>
2023-08-08 07:38:42 -0400

    47. Component State

--- components/ShareButtonLink.jsx
+++ components/ShareButtonLink.jsx
@@ -1,16 +1,26 @@
 'use client';
 
+import { useState } from "react";
+
 function ShareButtonLink() {
 
+    const [clicked, setClicked] = useState(false);
+
     const handleClick = () => {
-        console.log('Handle Click!');
+        navigator.clipboard.writeText(window.location.href);
+        setClicked(true);
+        setTimeout(() => {
+            setClicked(false);
+        }, 1500);
     };
+
+    console.log('Clicked State: ', clicked);
 
     return (
         <button className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
             onClick={handleClick}
         >
-            ShareLink
+            {clicked ? 'Link Copied' : 'ShareLink'}
         </button>
     );
 }
